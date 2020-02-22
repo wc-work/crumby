1db8e64c4c250a7221c608b5e68fe5a2887924d4
neptali04 <monteznepti@outlook.com>
2020-02-21 16:26:21 -0800

    Started badic structure of blog archive page.

---
+++ page-archive-blog.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+        <div class="row">
+        <div class="col-md-12">
+        <img src="" alt="">
+        
+        </div>
+        </div>
+        </section>
+		<!-- /section -->
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
