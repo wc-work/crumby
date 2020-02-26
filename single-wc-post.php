bfbaec56e70d1fa2f166091544bdb7cbf514d998
SolNat99 <natalieas01@gmail.com>
2020-02-25 16:52:42 -0800

    Fixing feature image sizing

--- single-wc-post.php
+++ single-wc-post.php
@@ -11,36 +11,39 @@ get_header();  ?>
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                     <!-- article -->
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
+
                         <!-- post thumbnail -->
-                        <?php if (has_post_thumbnail()) : // Check if Thumbnail exists 
-                        ?>
-                            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-                                <?php the_post_thumbnail(); // Fullsize image for the single post 
-                                ?>
-                            </a>
-                        <?php endif; ?>
-                        <!-- /post thumbnail -->
-                      
-                     <div class="container-fluid">        
-                        <!-- post details -->
-                        <span class="author"><?php _e('By', 'html5blank'); ?> <?php the_author_posts_link(); ?></span> |
-                        <span class="date"><?php the_time('F j, Y'); ?></span>
-                        <!-- post title -->
-                        <div class="post-title">
-                            <h1>
-                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                            </h1>
+                        <div class="image-holder">
+                            <?php if (has_post_thumbnail()) : // Check if Thumbnail exists 
+                            ?>
+                                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                                    <?php the_post_thumbnail(); // Fullsize image for the single post 
+                                    ?>
+                                </a>
+
+                            <?php endif; ?>
                         </div>
-                        <!-- /post title -->
-                        <!-- /post details -->
-                        <?php the_content(); // Dynamic Content 
-                        ?>
-                        <?php the_tags(__('Tags: ', 'html5blank'), ', ', '<br>'); // Separated by commas with a line break at the end 
-                        ?>
-                        <p><?php _e('Categorised in: ', 'html5blank');
-                            the_category(', '); // Separated by commas 
-                            ?></p>
+                        <!-- /post thumbnail -->
+
+                        <div class="container-fluid">
+                            <!-- post details -->
+                            <span class="author"><?php _e('By', 'html5blank'); ?> <?php the_author_posts_link(); ?></span> |
+                            <span class="date"><?php the_time('F j, Y'); ?></span>
+                            <!-- post title -->
+                            <div class="post-title">
+                                <h1>
+                                    <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                                </h1>
+                            </div>
+                            <!-- /post title -->
+                            <!-- /post details -->
+                            <?php the_content(); // Dynamic Content 
+                            ?>
+                            <?php the_tags(__('Tags: ', 'html5blank'), ', ', '<br>'); // Separated by commas with a line break at the end 
+                            ?>
+                            <p><?php _e('Categorised in: ', 'html5blank');
+                                the_category(', '); // Separated by commas 
+                                ?></p>
                     </article>
                     <!-- /article -->
                 <?php endwhile; ?>
@@ -54,7 +57,7 @@ get_header();  ?>
         </section>
         <!-- /section -->
     </main>
-    </div>
-</div><!-- end post-type -->
+</div><!-- /container-fluid -->
+</div><!-- /post-setup -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
