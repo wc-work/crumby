4b570f92ac930ffcd5e2df144bb2a7370fa79c73
SolNat99 <natalieas01@gmail.com>
2020-03-04 13:51:44 -0800

    Centered the contact form

--- page-contact-us.php
+++ page-contact-us.php
@@ -37,8 +37,9 @@ get_header(); ?>
 
     </div> <!-- end row -->
 
+    <div id ="center-form">
         <?php echo do_shortcode( '[contact-form-7 id="23" title="Contact form 1"]' ); ?>
-        <?php echo do_shortcode('[contact-form-7 id="92" title="Test Form"]', array('class' => 'd-flex justify-content-center')); ?>
+    </div>
       
 </div> <!-- end container -->
 <?php get_footer(); ?>
