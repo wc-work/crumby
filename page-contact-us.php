667739be88c6b3bad7189ac535cbfa758cbb70e1
Crystal Maldonado <howdycrystalm@gmail.com>
2020-03-04 21:54:21 -0800

    Cleaned up spacing

--- page-contact-us.php
+++ page-contact-us.php
@@ -15,25 +15,15 @@ get_header(); ?>
             <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
         </div>
 
-
         <div class="icon-text-container col-xs col-lg-4">
             <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="An icon that depicts an envelope">
             <h2> info.gkw.wp@gmail.com </h2>
         </div>
 
-
-
         <div class="icon-text-container col-xs col-lg-4">
             <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="An icon that depicts a cellphone">
             <h2> ###-###-####</h2>
         </div>
-
-        
-
-        <!--<div class="icon-text-container col-xs col-12 text-center">
-            <h2>Have ideas? Send them to Us!</h2>
-            <button type="button" class="btn btn-primary btn-lg">Send Ideas</button>
-        </div>-->
 
     </div> <!-- end row -->
 
