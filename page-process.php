077cf62241f7dddcd9a83e02d8dbf39c386db00b
Cruz Ramirez <cruz_ramirez@ymail.com>
2020-03-10 15:23:04 -0700

    added page-process php and scss file + starting code

---
+++ page-process.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Template Name: Process Template
+ * Template Post Type: page
+ */
+get_header();?>
+<main class="process">
+	<div class="d-flex flex-row flex-wrap">
+		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
+		<div class="flow_chart_row">
+			<div class="text col-6 col-lg-10 m_auto">
+				<h2>Discover</h2>
+				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
+				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
+			</div>
+			<div class="col-6 hidden_md">
+				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
+			</div>
+		</div>
+		<!--Loop ends here   -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
+		<div class="flow_chart_row">
+			<div class="text col-6 col-lg-10 m_auto">
+				<h2>Discover</h2>
+				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
+				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
+			</div>
+			<div class="col-6 hidden_md">
+				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
+			</div>
+		</div>
+		<!--Loop ends here   -->
+		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
+		<div class="flow_chart_row">
+			<div class="text col-6 col-lg-10 m_auto">
+				<h2>Discover</h2>
+				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
+				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
+			</div>
+			<div class="col-6 hidden_md">
+				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
+			</div>
+		</div>
+		<!--Loop ends here   -->
+		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
+		<div class="flow_chart_row">
+			<div class="text col-6 col-lg-10 m_auto">
+				<h2>Discover</h2>
+				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
+				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
+			</div>
+			<div class="col-6 hidden_md">
+				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
+			</div>
+		</div>
+		<!--Loop ends here   -->
+
+	</div>
+</main>
+
+<?php get_footer(); ?>
