6ef6db1f165bcffed48659065f6db206f02fe7e4
Cruz Ramirez <cruz_ramirez@ymail.com>
2020-03-10 16:36:52 -0700

    no more fixed on footer added sass for process page + made process page dynamic

--- page-process.php
+++ page-process.php
@@ -5,70 +5,40 @@
  */
 get_header();?>
 <main class="process">
-	<div class="d-flex flex-row flex-wrap">
-		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
-		<div class="flow_chart_row">
-			<div class="text col-6 col-lg-10 m_auto">
-				<h2>Discover</h2>
-				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
-				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
+	<!-- Banner -->
+	<section class="banner">
+		<div class="title"><?php the_title();?></div>
+		<div class="layer"></div>
+		<img src="<?php echo get_the_post_thumbnail_url();?>">
+	</section>
+	<!-- end Banner -->
+	<!-- Our Story -->
+	<div class="container">
+		<div class="d-flex flex-row flex-wrap">
+			<?php
+			// check if the repeater field has rows of data
+			if( have_rows('flow_info') ):
+				// loop through the rows of data
+				while ( have_rows('flow_info') ) : the_row();
+			?>
+			<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
+			<div class="flow_chart_row">
+				<div class="text col-6 col-lg-10 m_auto">
+					<h2><?php the_sub_field('title'); ?></h2>
+					<div class="icon_circle hidden_md_down"><i class="<?php the_sub_field('fa_icon'); ?>"></i></div>
+					<h3><?php the_sub_field('description'); ?></h3>
+				</div>
+				<div class="col-6 hidden_md">
+					<div class="icon_circle"><i class="<?php the_sub_field('fa_icon'); ?>"></i></div>
+				</div>
 			</div>
-			<div class="col-6 hidden_md">
-				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
-			</div>
-		</div>
-		<!--Loop ends here   -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
-		<div class="flow_chart_row">
-			<div class="text col-6 col-lg-10 m_auto">
-				<h2>Discover</h2>
-				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
-				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
-			</div>
-			<div class="col-6 hidden_md">
-				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
-			</div>
-		</div>
-		<!--Loop ends here   -->
-		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
-		<div class="flow_chart_row">
-			<div class="text col-6 col-lg-10 m_auto">
-				<h2>Discover</h2>
-				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
-				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
-			</div>
-			<div class="col-6 hidden_md">
-				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
-			</div>
-		</div>
-		<!--Loop ends here   -->
-		<!--Loop starts here if you decide to use a loop and a repeater for the ACF-->
-		<div class="flow_chart_row">
-			<div class="text col-6 col-lg-10 m_auto">
-				<h2>Discover</h2>
-				<div class="icon_circle hidden_md_down"><i class="fas fa-lightbulb fa-5x"></i></div>
-				<p>Meet with our team of professionals and find out how we can help you accomplish your goals.</p>
-			</div>
-			<div class="col-6 hidden_md">
-				<div class="icon_circle"><i class="fas fa-lightbulb fa-5x"></i></div>
-			</div>
-		</div>
-		<!--Loop ends here   -->
-
+			<!--Loop ends here   -->
+			<?php
+				endwhile;
+			else :
+				// no rows found
+			endif;
+			?>
 	</div>
 </main>
 
