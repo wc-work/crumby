5de8d91b7072f8f974c581771797969a34761145
faith orozco <faith_orozco@yahoo.com>
2020-03-11 15:03:16 -0700

    added col-sm-6 to contact form wrapper because when I merged the contact form was changed

--- page-contact-us.php
+++ page-contact-us.php
@@ -28,7 +28,7 @@ get_header(); ?>
 
         </div> <!-- end row -->
 
-    <div class="form-wrapper">
+    <div class="form-wrapper col-sm-6">
         <div class ="center-form">
             <?php echo do_shortcode( '[contact-form-7 id="21" title="Contact form 1"]' ); ?>
         </div>
