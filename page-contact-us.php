99a4e7063676b2b26d135cacc24517496332de12
Crystal Maldonado <howdycrystalm@gmail.com>
2020-03-25 14:23:59 -0700

    Updated page-contact-us.php to old code from branch 55-contact-center

--- page-contact-us.php
+++ page-contact-us.php
@@ -5,36 +5,36 @@
  */
 get_header(); ?>
 
-    <!--Contact Info Rows-->
-    <div class="contact-page">
-        <h1 class="contact-title">Contact Us!</h1>
-        <div class="container-fluid contact-us">
-            <div class="row row1">
-
-                <!-- icon-text-container is to help align the icons and text together -->
-                <div class="icon-text-container col-xs col-xl-12">
-                    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png" alt="An icon that symbolizes locations">
-                    <h2><?php the_field('locaction_info'); ?> </h2>
-                </div>
-
-                <div class="icon-text-container col-xs col-xl-12">
-                    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="An icon that depicts an envelope">
-                    <h2> <?php the_field('email_info'); ?> </h2>
-                </div>
-
-                <div class="icon-text-container col-xs col-xl-12">
-                    <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="An icon that depicts a cellphone">
-                    <h2><?php the_field('phone_info'); ?></h2>
-                </div>
-
-            </div> <!-- end row -->
-
-            <div class="form-wrapper">
-                <div class ="center-form">
-                    <?php echo do_shortcode( '[contact-form-7 id="21" title="Contact form 1"]' ); ?>
-                </div>
+<!--Contact Info Rows-->
+<div class="contact-page">
+    <h1 class="contact-title"> Contact Us!</h1>
+    <div class="container-fluid contact-us row">
+        <div class="row icon-wrapper col-sm-6">
+        <!-- icon-text-container is to help align the icons and text together -->
+            <div class="icon-text-container col-xs col-xl-12">
+                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/location-solid.png" alt="An icon that symbolizes locations">
+                <h2> 2721 Ventura St, Fresno, CA 93721 </h2>
             </div>
-        
-        </div> <!-- end container -->
-    </div>
+
+            <div class="icon-text-container col-xs col-xl-12">
+                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/email.png" alt="An icon that depicts an envelope">
+                <h2> info.gkw.wp@gmail.com </h2>
+            </div>
+
+            <div class="icon-text-container col-xs col-xl-12">
+                <img class="icons" src="<?php bloginfo('template_url'); ?>/img/icons/phone.png" alt="An icon that depicts a cellphone">
+                <h2> 559-906-8945</h2>
+            </div>
+
+        </div> <!-- end row -->
+
+        <div class="form-wrapper col-sm-6">
+            <div class ="center-form">
+                 <?php echo do_shortcode( '[contact-form-7 id="23" title="Contact form 1"]' ); ?>
+            </div>
+        </div>
+     
+    </div> <!-- end container -->
+    
+</div>
 <?php get_footer(); ?>
