3a730fb53b8ea37b85acd7c38ab7ff22ac90874b
SolNat99 <natalieas01@gmail.com>
2020-06-24 15:37:59 -0700

    Inital commit

--- blog.php
+++ blog.php
@@ -15,8 +15,8 @@ get_header(); ?>
             <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
                 <!-- <div class="layer"></div>
                 <div class="blog-img layout mb-3" style="background-image:url(<?php echo get_the_post_thumbnail_url();?>);">
-                    <div class="blog-title"> <?php the_title(); ?></div>
-                    <div class="excerpt"><?php echo get_excerpt(140); ?></div>
+                    <div class="blog-title p-5"> <?php the_title(); ?></div>
+                    <div class="excerpt px-5"><?php echo get_excerpt(140); ?></div>
                     <a href="<?php the_permalink();?>"><button type="button" class="btn btn-lg blog-button">Read More</button></a>
                 </div> -->
                 <?php get_template_part('partials/blog-banner');?>
