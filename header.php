9e9bd7adf7af36680434d8c844d70571051b5462
Crystal Maldonado <howdycrystalm@gmail.com>
2020-07-06 21:47:11 -0700

    Added Google Tab Manager

--- header.php
+++ header.php
@@ -25,6 +25,13 @@
 			gtag('config', 'UA-171812489-1');
 		</script>
 		<!-- END Global site tag (gtag.js) - Google Analytics -->
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WDGT6F7');</script>
+		<!-- End Google Tag Manager -->
 		<?php wp_head(); ?>
 			<script>
 				// conditionizr.com
@@ -38,6 +45,10 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDGT6F7"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
 	<nav class="navbar navbar-expand-md navbar-light sticky-top standard" role="navigation">
     <div class="container-fluid" id="nav-icon">
