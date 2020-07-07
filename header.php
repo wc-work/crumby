ac9c3cd5101f3549e2964694a2a3bf5351bd9f49
Crystal Maldonado <howdycrystalm@gmail.com>
2020-07-06 21:13:48 -0700

    Added google analytics tag

--- header.php
+++ header.php
@@ -15,7 +15,16 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-171812489-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
 
+			gtag('config', 'UA-171812489-1');
+		</script>
+		<!-- END Global site tag (gtag.js) - Google Analytics -->
 		<?php wp_head(); ?>
 			<script>
 				// conditionizr.com
